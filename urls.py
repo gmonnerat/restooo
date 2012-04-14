d43a3467fdcfdd1728f0450c1f516dca6175f02c
Gabriel Monnerat <gabrielmonnerat@gmail.com>
2012-04-14 16:46:37 -0300

    load files from static folder

--- urls.py
+++ urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.conf import settings
 
 urlpatterns = patterns('',
+    (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
     url(r'^$', include('cloudooo.urls')),
 )
